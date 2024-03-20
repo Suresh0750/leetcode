55987552ef5c06c1fa2614018c436d69fb8990e1
Suresh0750 <sureshvarma@gmail.com>
2024-03-19 21:11:34 -0700

    calculate power value

---
+++ leetcode 231.js
@@ -0,0 +1,28 @@
+// Example 1:
+
+// Input: n = 1
+// Output: true
+// Explanation: 2^0 = 1
+
+
+
+var isPowerOfTwo = function(n) {
+
+    let i=0
+    
+    
+    do{
+         
+        if(Math.pow(2,i) == n){
+           
+            return true
+        }else if(Math.pow(2,i) > n){
+          
+            return false
+        }
+        i++
+        
+        
+    }while(i<n)
+    
+    };
