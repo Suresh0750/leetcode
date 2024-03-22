c6ae2d70371423755da5c213866178fef4e23b64
Suresh0750 <sureshvarma@gmail.com>
2024-03-21 21:52:21 -0700

    repeatedly add all its digits until the result has only one digit

---
+++ leetcode 258.js
@@ -0,0 +1,52 @@
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+
+
+
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+
+
+    //first method
+    // while(num>9){
+    //     num=num%10+Math.floor(num/10)
+    // }
+    // return num
+
+
+
+// second method
+ function add (num){
+    num = String(num)
+    let sum = 0
+    let i=0
+    while(i<num.length){
+        
+        sum += Number(num[i])
+        
+        i++
+    }
+        
+    if(String(sum).length !=1){
+      let call =  add(sum) 
+      return call
+    }else{
+      return sum
+    }
+    
+}
+
+let result = add(num)
+
+return result
+
+};
