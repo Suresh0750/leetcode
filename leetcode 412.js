a055faed4572cf410630f02b311337fed64fb2d6
Suresh0750 <sureshvarma0750@gmail.com>
2024-04-22 20:56:18 -0700

    Fizz Buzz

---
+++ leetcode 412.js
@@ -0,0 +1,24 @@
+
+var fizzBuzz = function(n) {
+
+    let i = 1
+    let final = []
+    while(i<=n){
+
+        if(i%3==0 & i%5==0){
+            final.push("FizzBuzz")
+        }else if(i%3==0){
+            final.push("Fizz")
+        }else if(i%5==0){
+            final.push('Buzz')
+        }else{
+
+            final.push(`${i}`)
+        }
+
+        i++
+    }
+
+    return final
+    
+};
