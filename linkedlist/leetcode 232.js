6d402eded401f5253908f1b522717d8e1b5af082
Suresh0750 <sureshvarma0750@gmail.com>
2024-04-27 07:47:18 -0700

    Implement Queue using Stacks

---
+++ linkedlist/leetcode 232.js
@@ -0,0 +1,46 @@
+
+var MyQueue = function() {
+    this.array = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.array.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.array.length) return this.array.shift()
+
+    return undefined
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if(this.array.length) return this.array[0]
+
+    return undefined
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.array.length == 0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
