d71d93587bb6b97a277a6af7f46291937002eb46
Suresh0750 <sureshvarma0750@gmail.com>
2024-04-28 21:41:16 -0700

    count even number

---
+++ leetcode 2180.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+
+
+    let c = 0
+    let i = 1
+    let sum = 0
+    let value
+    while(i<=num){
+        sum = 0
+        value = i
+        while(value){
+            sum+=value%10
+            value = Math.floor(value/10)
+        }
+        
+        if(sum%2==0){
+            c++
+        }
+        i++
+    }
+    
+    return c
+    
+    };
