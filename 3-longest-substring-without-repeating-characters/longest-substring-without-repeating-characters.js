bb94b5aff8a9dfb0efb14620ecfb4fa0251483bb
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-08-06 19:02:34 +0530

    Time: 251 ms (10.83%) | Memory: 57.9 MB (10.54%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+
+if(s.length<2) return s.length
+
+let i =0 
+
+let j =0
+let max = 0
+
+while(j<s.length){
+    
+      
+    if(s.substr(i,(j-i)).includes(s[j])){
+        let newSub = s.substr(i,(j-i))
+        console.log(s.substr(i,(j-i)).length,newSub,s[j])
+        max = Math.max(s.substr(i,(j-i)).length,max)
+       i++
+       j--
+    }
+    j++
+}
+
+ max = Math.max(s.substr(i,(j-i)).length,max)
+
+console.log(max)
+
+return max
+};
