798bc4a0daeffa712708f5c221727da9dfe5ea51
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-08-10 22:02:50 +0530

    Time: 2976 ms (5.02%) | Memory: 58.3 MB (14.78%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,57 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+
+if(s.length==1) return s
+
+let res = ''
+
+let i=0
+
+let obj = {}
+
+while(i<s.length){
+    if(obj[s[i]]){
+       obj[s[i]].push(i)
+    }else{
+        obj[s[i]] = [i]
+    }
+    i++
+}
+
+function isPalindrome(sub){
+    // console.log('sub',sub)
+    return sub==sub.split('').reverse('').join('')
+}
+function findSubPalindrome(i,k,arr){
+    return s.substr(arr[i],((arr[k]-arr[i])+1))
+}
+for(let key in obj){
+    
+    if(obj[key].length>1){
+        let arr = obj[key]
+        // console.log(key,obj[key])
+        let i=0
+      while(i<arr.length){
+        for(let k=arr.length-1;k>i;k--){
+            let subPalindrome = s.substr(arr[i],((arr[k]-arr[i])+1))
+            if(subPalindrome.length>res.length){
+             let sub = isPalindrome(subPalindrome)
+             if(sub) {
+                let val = s.substr(arr[i],((arr[k]-arr[i])+1))
+                res = val
+            }
+            }
+        }
+        i++
+      }
+    }
+    
+}
+
+return res.length==0 ? s[0] : res
+   
+    
+};
