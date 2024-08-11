a46ffc96508ff310035f81d23c206d966930dd44
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-08-12 00:29:06 +0530

    Time: 49 ms (68.87%) | Memory: 48.8 MB (69.76%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -8,7 +8,7 @@ let container = ['','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
 if(digits.length==1) return container[digits[0]-1].split('')
 
 let i = 0
-console.log(container[digits[0]-1])
+
 let total = 1
 while(i<digits.length){
     total*=container[digits[i]-1].length
@@ -37,15 +37,12 @@ i = 0
 
 function call(m,j,k,z){
     
-    if(digits.length==1){
-       
-      return insideContain[0][(((i/1)%(z+1)))]
-    }else if(digits.length==2) {
-        console.log(k,z,i,((Math.floor(i/3)%(k+1))),((((i/1)%(z+1)))))
-        return insideContain[0][((Math.floor(i/k)%(k+1)))] +insideContain[1][((Math.floor(i/1)%(z)))]
+if(digits.length==2) {
+        // console.log(k,z,i,((Math.floor(i/3)%(k+1))),((((i/1)%(z+1)))))
+        return insideContain[0][((Math.floor(i/z)%(k)))] +insideContain[1][((Math.floor(i/1)%(z)))]
     }else if(digits.length==3){
-console.log(j,k,z,((Math.floor(i/(j*k))%(j))),((Math.floor(i/k)%(k))),((Math.floor(i/1)%(z))))
-        return insideContain[0][((Math.floor(i/(j*k))%(j)))]+insideContain[1][((Math.floor(i/k)%(k)))] +insideContain[2][((Math.floor(i/1)%(z)))]
+// console.log(j,k,z,((Math.floor(i/(j*k))%(j))),((Math.floor(i/k)%(k))),((Math.floor(i/1)%(z))))
+        return insideContain[0][((Math.floor(i/(z*k))%(j)))]+insideContain[1][((Math.floor(i/k)%(k)))] +insideContain[2][((Math.floor(i/1)%(z)))]
     }else if(digits.length==4){
         return insideContain[0][((Math.floor(i/(z*k*j))%(m)))]+insideContain[1][((Math.floor(i/(z*k))%(j)))]+insideContain[2][((Math.floor(i/z)%(k)))] +insideContain[3][((Math.floor(i/1)%(z)))]
     }
@@ -54,11 +51,7 @@ console.log(j,k,z,((Math.floor(i/(j*k))%(j))),((Math.floor(i/k)%(k))),((Math.flo
 
 let res = []
 while(total){
-    
-    if(digits.length==1){
-        res.push(call(0,0,0,insideContain[0].length))
-        
-    }else if(digits.length==2){
+ if(digits.length==2){
          res.push(call(0,0,insideContain[1].length,insideContain[0].length))
         
     }else if(digits.length==3){
