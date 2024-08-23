0c01e54cb364ddea86989d46148e0806ea1acb87
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-08-23 09:35:28 +0530

    Time: 34 ms (99.42%) | Memory: 49.6 MB (12.28%) - LeetSync

---
+++ 1289-day-of-the-week/day-of-the-week.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    
+    return days[(new Date(year,month-1,day).getDay())]
+};
