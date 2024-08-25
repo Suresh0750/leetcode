f9b4d86ef1e00cc30c1241fedddfb42b985b54cf
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-08-25 23:40:37 +0530

    Time: 50 ms (64.39%) | Memory: 48.9 MB (47.17%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -14,18 +14,13 @@ while(i<digits.length){
     total*=container[digits[i]-1].length
     i++
 }
-
 function containerfun(digits){
     if(digits.length==1){
         return [container[digits[0]-1]]
     }else if(digits.length==2){
-        
         return [container[digits[0]-1],container[digits[1]-1]]
-
     }else if(digits.length==3){
-
         return [container[digits[0]-1],container[digits[1]-1],container[digits[2]-1]]
-        
     }else if(digits.length==4){
         return [container[digits[0]-1],container[digits[1]-1],container[digits[2]-1],container[digits[3]-1]]
     }
@@ -44,17 +39,14 @@ if(digits.length==2) {
     }else if(digits.length==4){
         return insideContain[0][((Math.floor(i/(z*k*j))%(m)))]+insideContain[1][((Math.floor(i/(z*k))%(j)))]+insideContain[2][((Math.floor(i/z)%(k)))] +insideContain[3][((Math.floor(i/1)%(z)))]
     }
-  
 }
 
 let res = []
 while(total){
  if(digits.length==2){
          res.push(call(0,0,insideContain[1].length,insideContain[0].length))
-        
     }else if(digits.length==3){
-         res.push( call(0,insideContain[2].length,insideContain[1].length,insideContain[0].length))
-        
+         res.push( call(0,insideContain[2].length,insideContain[1].length,insideContain[0].length))      
     }else if(digits.length==4){
          res.push(call(insideContain[0].length,insideContain[1].length,insideContain[2].length,insideContain[3].length))
     }
