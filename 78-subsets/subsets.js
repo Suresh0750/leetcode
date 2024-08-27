089478119af6f08292daaaeccbf4fb5ff519743d
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-08-28 05:00:56 +0530

    Time: 61 ms (58.39%) | Memory: 52.6 MB (44.86%) - LeetSync

---
+++ 78-subsets/subsets.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+
+let res = [[]]
+
+
+function backtracking(arr,i){
+    if(i>=nums.length) return arr
+    
+   
+   let newArr = []
+   
+   for(let j=0;j<arr.length;j++){
+       newArr.push(arr[j])
+       newArr.push([...arr[j],nums[i]])
+   }
+   
+    i+=1
+    return backtracking(newArr,i)
+}
+
+console.log(backtracking([[]],0))
+ return backtracking([[]],0)
+
+};
