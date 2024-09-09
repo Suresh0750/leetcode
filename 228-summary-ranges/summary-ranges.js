500ef67abd668571a116ee68a60c748b52b441c0
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-09-09 10:19:33 +0530

    Time: 59 ms (11.23%) | Memory: 48.5 MB (83.92%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+   let store = nums[0]
+
+    let i = 0
+    let res = []
+    while(i<nums.length){
+        if(nums[i]+1==nums[i+1]){
+        i++
+        continue
+        }else if(nums[i]+1!=nums[i+1]){
+            if(store==nums[i]) res.push(`${nums[i]}`)
+            else res.push(`${store}->${nums[i]}`)
+            store = nums[i+1]
+        }
+        i++
+    }
+ return res 
+};
