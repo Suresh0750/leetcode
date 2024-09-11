9862b4731e1f8e7a03dccf0e846bdb2cb5256e88
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-09-11 09:44:39 +0530

    Time: 216 ms (42.27%) | Memory: 55.1 MB (39.18%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.js
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.js
@@ -6,30 +6,24 @@
 var countPrimeSetBits = function(left, right) {
 
  function isPrime(cin){
-        
         let i = 2
         while(i<=cin){
-            
             if(cin%i==0) break;
-            
             i++
         }
         if(cin==i) c++
-    //  console.log(cin, c)
     }
     
     let c= 0
     let cin = 0
     while(left<=right){
         let bits = ((left).toString(2))+''
-      
         cin = 0
         for(let i=0;i<bits.length;i++){
             if(bits[i]==1){
                 cin++
             }
         }
-        //   console.log(bits, cin)
         isPrime(cin)
         left++
     }
