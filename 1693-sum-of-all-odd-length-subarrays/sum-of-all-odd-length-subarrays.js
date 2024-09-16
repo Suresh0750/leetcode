192c4ebbb135bda973a6d0b16ed8dbb3019cb8cd
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-09-16 10:12:24 +0530

    Time: 57 ms (50.49%) | Memory: 55 MB (20.59%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
@@ -6,14 +6,13 @@ var sumOddLengthSubarrays = function(arr) {
 
 let sum = 0
 let j =1 
-// let i = 0
+
 while(j<arr.length){
     let z =0 
     while(z<arr.length){
-        // console.log(arr.slice(z,j+z))
+       
         if(arr.slice(z,j+z).length==j){
              sum += arr.slice(z,j+z).reduce((acc,curr)=>  acc+curr)
-             console.log(arr.slice(z,j+z).reduce((acc,curr)=>  acc+curr))
         }
         z++
     }
