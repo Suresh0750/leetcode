193f039e14f3e3a2b1bb1d5faa3dfaac9cf9d6fc
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-09-23 09:38:06 +0530

    Time: 56 ms (32.61%) | Memory: 50.8 MB (9.51%) - LeetSync

---
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let newArr = []
+
+    nums.map((val,i)=>{
+        if(!newArr[index[i]] && newArr[index[i]]!=0){
+          newArr[index[i]] = val 
+        }else{
+            newArr.splice(index[i],0,val)
+        }
+    })
+
+    console.log(newArr)
+    return newArr
+};
