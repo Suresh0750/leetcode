433b7c9e94527be28955fdbe2a3416418f353385
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-10-13 11:24:50 -0700

    Time: 61 ms (41.36%) | Memory: 51.3 MB (39.81%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -6,11 +6,11 @@
 var canPlaceFlowers = function(flowerbed, n) {
     let c = 0
    for(let i=0;i<flowerbed.length;i++){
-    if(flowerbed[i]!=1&&flowerbed[i+1]!=1 && flowerbed[i-1]!=1){
-        c++
-        i++
+        if(flowerbed[i]!=1&&flowerbed[i+1]!=1 && flowerbed[i-1]!=1){
+            c++
+            i++
+        }
     }
-}
-    console.log(c)
+    // console.log(c)
     return c>=n
 };
