ee8bfddee2405ca2a687574e9bf6703bdb85d443
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-11-09 22:40:44 +0530

    Time: 47 ms (27.55%) | Memory: 64.5 MB (15.11%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,60 +3,29 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+ 
+// [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-obj ={}
+let newStrs = strs.map((val)=> val.split('').sort().join(''))
 
+let obj = {}
 let i = 0
-
-
-while(i<strs.length){
-    let sortVal = strs[i].split('').sort().join('')
-    if(obj[sortVal]){
-     obj[sortVal].push(strs[i])
+while(i<newStrs.length){
+    if(obj[newStrs[i]]){
+        obj[newStrs[i]].push(i)
     }else{
-        obj[sortVal] = [strs[i]]
+        obj[newStrs[i]] = [i]
     }
     i++
 }
-return Object.values(obj)
+let res = []
+ Object.values(obj).map((val)=>{
+     let inArr = []
+   val.map((v1)=>{
+       inArr.push(strs[v1])
+   })
+   res.push(inArr)
+})
 
-
-
-
-
-
-
-
-
-// console.log(Object.values(obj))
-
-// console.log(obj)
-    
-// let newStrs = strs.map((val)=>{
-//     return val.split('').sort().join('')
-// })
-
-// let res = []
-
-// let i=newStrs.length-1
-
-// while(i>=0){
-//     let inarr = []
-//     inarr.push(strs[i])
-//     j = i-1
-//     if(strs[i]!=null){
-//     while(j>=0){
-//         if(newStrs[i]==newStrs[j]){
-//             inarr.push(strs[j])
-//             strs[j]=null
-//         }
-//         j--
-//     }
-//     res.push(inarr)
-//     }
-//     i--
-// }
-
-//  return res
-
+return res
 };
