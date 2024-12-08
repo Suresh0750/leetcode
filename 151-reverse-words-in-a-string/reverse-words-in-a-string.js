8dc797225f7fded7b6c9810c05045d9c13da71c0
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-12-08 13:08:22 +0530

    Time: 3 ms (49.67%) | Memory: 51.3 MB (30.26%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,14 +3,5 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    
-    s = s.split(' ')
-    let i= s.length-1
-    let res = ''
-    while(i>=0){
-       if(s[i].length>0) res+=s[i]+' '
-       i--
-
-    }
-    return res.trim()
+     return s.split(" ").filter(word => word != "").reverse().join(" ")
 };
