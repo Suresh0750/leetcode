01dc7a28e746e60c4b13eeb55a594c78dd600b1c
Suresh0750 <146124181+Suresh0750@users.noreply.github.com>
2024-12-12 09:57:47 +0530

    Time: 2 ms (72.07%) | Memory: 53.3 MB (74.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,58 +3,38 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-   
-let findZero = []
-     
-     function zeroAss(i,j){
-         
-         
-         for(let z=0;z<matrix[i].length;z++){
-             if(matrix[i][z]==0){
-                 findZero([i,z])
-             }
-             matrix[i][z] = 0
-         }
-         
-         for(let z = 0;z<matrix.length;z++){
-             if(matrix[z][j]==0){
-                 findZero.push(z,j)
-             }
-             matrix[z][j] = 0
-         }
-     }
-     function secondAss(i,j){
-         
-         
-         for(let z=0;z<matrix[i].length;z++){
-             matrix[i][z] = 0
-         }
-         
-         for(let z = 0;z<matrix.length;z++){
-             
-             matrix[z][j] = 0
-         }
-     }
 
-    let i=0
-    while(i<matrix.length){
+let row = new Set()
+let col = new Set()
 
-        if(matrix[i].includes(0)){
-             for(let j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==0){
-                    findZero.push([i,j])
-                }
-            }
+let i = 0
+
+
+while(i<matrix.length){
+    let j = 0
+    while(j<matrix[i].length){
+        // console.log(matrix[i][j],i,j)
+        if(matrix[i][j]==0){
+            row.add(i)
+            col.add(j)
         }
-        i++
+        j++
     }
-    
-    i = 0
-    while(i<findZero.length){
-        secondAss(findZero[i][0],findZero[i][1])
-        i++
+    i++
+}
+
+
+ i =0 
+while(i<matrix.length){
+    let j = 0
+    while(j<matrix[i].length){
+        if(row.has(i) || col.has(j)){
+            matrix[i][j]=0
+        }
+        j++
     }
-    return matrix
-     
-   
+    i++
+}
+
+ return matrix
 };
