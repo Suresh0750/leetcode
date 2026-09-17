862cf0d21670fed9c41860c0a4921f5a0a091b98
Suresh M <146124181+Suresh0750@users.noreply.github.com>
2026-09-17 17:31:03 +0530

    Time: 1 ms (98.11%) | Memory: 42 MB (98.59%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,16 +1,19 @@
 class Solution {
     public int addDigits(int num) {
-        return helper(num,0);
+        return num==0 ? 0 : num%9==0 ? 9 : num%9;
     }
-    public int helper(int num, int cal){
-        cal += num%10;
-        int nextInt = num/10;
-        if(nextInt==num && cal%10==cal){
-            return cal;
-        }else if(nextInt==num){
-            nextInt = cal;
-            cal = 0;
-        }
-        return helper(nextInt,cal);
-    }
+    // public int addDigits(int num) {
+    //     return helper(num,0);
+    // }
+    // public int helper(int num, int cal){
+    //     cal += num%10;
+    //     int nextInt = num/10;
+    //     if(nextInt==num && cal%10==cal){
+    //         return cal;
+    //     }else if(nextInt==num){
+    //         nextInt = cal;
+    //         cal = 0;
+    //     }
+    //     return helper(nextInt,cal);
+    // }
 }
