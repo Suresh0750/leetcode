499eb5885b73f58f4d26fe6961a8afe9cf2f31e2
Suresh M <146124181+Suresh0750@users.noreply.github.com>
2026-09-21 11:29:14 +0530

    Add Easy JavaScript Solution for 26. Remove Duplicates from Sorted Array from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/26. Remove Duplicates from Sorted Array/Solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+let i=0
+
+while(i<nums.length){
+    let j=i+1
+    while(j<nums.length){
+        
+        if(nums[i] == nums[j]){
+            
+            nums.splice(j,1)
+            j--
+        }
+        j++
+    }
+    
+    i++
+}
+
+
+return nums.length
+
+};
